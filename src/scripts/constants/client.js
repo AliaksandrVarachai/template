27c56e9a489eb86e72f45c25b187fd9c1f4df603
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-06-28 18:47:29 +0300

    Add constants

---
+++ src/scripts/constants/client.js
@@ -0,0 +1,13 @@
+/**
+ * Id of DOM element containing feedback tool form.
+ * @type {string}
+ */
+export const FEEDBACK_TOOL_ROOT_ID = 'feedback-root';
+
+/**
+ * Pairs of listened events and relevant DOM attributes.
+ * @type {{string: string}}
+ */
+export const OCS_EVENTS = {
+  'click': 'feedback-onclick',
+};
---
+++ src/scripts/constants/server.js
@@ -0,0 +1,9 @@
+/**
+ * Backend API settings.
+ * @type {{ORIGIN: string, PATH: string}}
+ */
+export const API_SETTINGS = {
+  // TODO: replace fake API params with real ones
+  ORIGIN: 'https://ecsb00100b6f.epam.com:480',
+  PATH: '/api'
+};
