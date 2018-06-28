bebd4ba17f518e0afe2603f51d3042b393f72ab3
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-06-28 13:08:27 +0300

    Add mocked html files

---
+++ resources/webpack-dev-server/tableau/index.js
@@ -0,0 +1,10 @@
+/**
+ * Provides testing of created components on Tableau local dev server.
+ * Copies html into output directory.
+ * @example <caption>Local testing of Tableau components.</caption>
+ * http://localhost:9091
+ * http://localhost:9091/index.html
+ */
+
+import './index.html';
+import './iframe-0.html';
