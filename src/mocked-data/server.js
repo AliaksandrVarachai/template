3d31ebeb97bc7f00013b5c52389ef3833419dce6
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-06-29 19:10:34 +0300

    Add mocked server data

---
+++ src/mocked-data/server.js
@@ -0,0 +1,3 @@
+/**
+ * Created by aliaksandr_varachai on 6/29/18.
+ */
--- src/scripts/constants/server.js
+++ src/scripts/constants/server.js
@@ -4,6 +4,22 @@
  */
 export const API_SETTINGS = {
   // TODO: replace fake API params with real ones
-  ORIGIN: 'https://ecsb00100b6f.epam.com:480',
-  PATH: '/api'
+  ORIGIN: 'http://ecsb00100b6f.epam.com',
+  PATH: '/FeedbackTool/api'
+};
+
+/**
+ * Settings for local test of the application.
+ * @type {{NAME: string, PORT: number}}
+ */
+const API_SETTINGS_LOCAL = {
+  NAME: 'localhost',
+  PORT: 9093,
+  PATH: API_SETTINGS.PATH
+};
+API_SETTINGS_LOCAL.ORIGIN = `${API_SETTINGS_LOCAL.NAME}:${API_SETTINGS_LOCAL.PORT}`;
+
+module.exports = {
+  API_SETTINGS,
+  API_SETTINGS_LOCAL,
 };
