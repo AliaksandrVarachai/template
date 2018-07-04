b394841dae913067014c1c8dd9d086b39349a1d6
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2018-07-04 15:11:33 +0300

    Simplify local api server logic

--- src/mocked-data/server.js
+++ src/mocked-data/server.js
@@ -3,20 +3,15 @@ const { API_SETTINGS_LOCAL } = require('../scripts/constants/server');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  switch(req.method) {
-    case 'GET':
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end('Get request is received');
-      break;
-    case 'POST':
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end('POST request is received');
-      break;
-    default:
-      // nothing
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('Get request is received');
+  } else if (req.method === 'POST') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('POST request is received');
+  } else {
+    console.log(`Method "${req.method} is not supported"`)
   }
-
-
 }).listen(API_SETTINGS_LOCAL.PORT, API_SETTINGS_LOCAL.HOSTNAME);
 
 console.log(`Local API server is started on ${API_SETTINGS_LOCAL.ORIGIN}`)
